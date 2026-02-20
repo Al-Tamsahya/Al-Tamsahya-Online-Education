8ee4252d44cae8e70ea65b5a1ff0da290284969b
Mohamed Alkhatib <m.abdelalim2017@gmail.com>
2026-02-20 03:30:23 +0300

    Update Lesson.cs

--- Lesson.cs
+++ Lesson.cs
@@ -1,5 +1,3 @@
-﻿using static System.Collections.Specialized.BitVector32;
-
 namespace EducationalPlatform.API.Models
 {
     public class Lesson
@@ -8,14 +6,7 @@ namespace EducationalPlatform.API.Models
         public string Title { get; set; } = string.Empty;
         public string? Content { get; set; }
 
-        // Added: video for each lesson
-        public string? VideoUrl { get; set; }
-
         public int SubjectId { get; set; }
-        public required Subject Subject { get; set; }
-        public int SectionId { get; set; }
-        public required Section Section { get; set; }
-
+        public Subject Subject { get; set; }
     }
 }
-
